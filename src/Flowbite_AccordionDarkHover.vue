5fe6ef39730f36e6400da54c54c3937048bb5bdf
zengxiaoqi <zengxiaoqi@gmail.com>
2025-10-12 23:36:20 +0800

    downgrade Flowbite-vue and tailwindcss version to introduce tailwind.config.cjs to control the dark mode; added a button to control dark mode

---
+++ src/Flowbite_AccordionDarkHover.vue
@@ -0,0 +1,45 @@
+<template>
+  <button @click="toggleDark" class="border p-2 rounded">
+    Toggle Dark Mode
+  </button>
+  <fwb-accordion>
+    <fwb-accordion-panel
+      v-for="(panel, i) in panels"
+      :key="i"
+      :class="{ 'border-b-0': i !== panels.length - 1 }"
+      class="border border-cyan-700"
+      @show="() => (panel.isVisible = true)"
+      @hide="() => (panel.isVisible = false)"
+    >
+      <fwb-accordion-header
+        class="w-full justify-center rounded-none border-0 bg-white hover:bg-cyan-200 dark:bg-cyan-800 dark:text-white dark:hover:bg-cyan-700 [&>svg]:m-0"
+        :active-class="'bg-lime-200 hover:bg-lime-300 dark:bg-lime-500 dark:text-black dark:hover:bg-lime-400'"
+      >
+        Item {{ i+1 }} ({{ panel.isVisible ? 'open' : 'closed' }})
+      </fwb-accordion-header>
+      <fwb-accordion-content class="border-t border-t-cyan-700 text-center ">
+        <p>{{ panel.content }}</p>
+      </fwb-accordion-content>
+    </fwb-accordion-panel>
+  </fwb-accordion>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {
+  FwbAccordion,
+  FwbAccordionContent,
+  FwbAccordionHeader,
+  FwbAccordionPanel,
+} from 'flowbite-vue'
+
+const panels = ref([
+  { isVisible: false, content: 'Lorem ipsum...' },
+  { isVisible: false, content: 'Lorem ipsum...' },
+  { isVisible: false, content: 'Lorem ipsum...' },
+])
+
+const toggleDark = () => {
+  document.documentElement.classList.toggle('dark')
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import Flowbite from './Flowbite.vue'
+// import Flowbite_Accordion from './Flowbite_Accordion.vue'
+import Flowbite_Accordion from './Flowbite_AccordionDarkHover.vue'
 
 // createApp(App).mount('#app')
-createApp(Flowbite).mount('#app')
+// createApp(Flowbite).mount('#app')
+createApp(Flowbite_Accordion).mount('#app')
