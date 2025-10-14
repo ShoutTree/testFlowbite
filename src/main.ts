0581e1e5ae44b23e6103f0751bc8bb374e1ce3de
zengxiaoqi <zengxiaoqi@gmail.com>
2025-10-14 10:07:11 +0800

    added Alert, but wrong color, wrong alignment, wrong appearance now.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import App from './App.vue'
 import Flowbite from './Flowbite.vue'
 // import Flowbite_Accordion from './Flowbite_Accordion.vue'
 import Flowbite_Accordion from './Flowbite_AccordionDarkHover.vue'
+import Flowbite_Alert from './Flowbite_Alert.vue'
+
 
 // createApp(App).mount('#app')
 // createApp(Flowbite).mount('#app')
-createApp(Flowbite_Accordion).mount('#app')
+// createApp(Flowbite_Accordion).mount('#app')
+createApp(Flowbite_Alert).mount('#app')
+
