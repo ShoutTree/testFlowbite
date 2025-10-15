1dffc8c5ff86b7c98ceabc37ff7ef46460053951
zengxiaoqi <zengxiaoqi@gmail.com>
2025-10-15 09:22:53 +0800

    avatars, to alt text. not succeeded in showing alt text.

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import Flowbite from './Flowbite.vue'
 // import Flowbite_Accordion from './Flowbite_Accordion.vue'
 import Flowbite_Accordion from './Flowbite_AccordionDarkHover.vue'
 import Flowbite_Alert from './Flowbite_Alert.vue'
+import Flowbite_Avatar from './Flowbite_Avatar.vue'
 
 
 // createApp(App).mount('#app')
 // createApp(Flowbite).mount('#app')
 // createApp(Flowbite_Accordion).mount('#app')
-createApp(Flowbite_Alert).mount('#app')
+// createApp(Flowbite_Alert).mount('#app')
+createApp(Flowbite_Avatar).mount('#app')
 
